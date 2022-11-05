f561e9ce63cde656d5fc2078456fbc051a52e6ae
morshidulrahman <boydanger416@gmail.com>
2022-11-05 16:02:42 +0600

    server_siderender

--- app/Components/index.js
+++ app/Components/index.js
@@ -1,2 +1,3 @@
 export { default as Header } from "./Hader"
 export { default as Banner } from "./Baner"
+export { default as ProductFeed } from "./Products/ProductFeed"
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
-import { Banner, Header } from "../app/Components"
+import { Banner, Header, ProductFeed } from "../app/Components"
 
-export default function Home() {
+export default function Home({ products }) {
   return (
-    <>
+    <div className="bg-gray-100">
       <Header />
       <main className="max-w-screen-2xl mx-auto">
         <Banner />
+        <ProductFeed products={products} />
       </main>
-    </>
+    </div>
   )
+}
+
+export async function getServerSideProps(contex) {
+  const products = await fetch('https://fakestoreapi.com/products/1')
+    .then(res => res.json())
+  return {
+    props: {
+      products
+    }
+  }
 }
