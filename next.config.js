25fc6df32aafa40af3aaa536bef175ba7736b55f
morshidulrahman <boydanger416@gmail.com>
2022-11-09 17:24:48 +0600

    firebase_config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    FIREBASE_API_KEY: "AIzaSyAfiHR5I2rcBr6rlfeOQp8rJzHlzDg8d2Q",
+    FIREBASE_AUTH_DOMAIN: "fir-56958.firebaseapp.com",
+    FIREBASE_PROJECT_ID: "fir-56958",
+    FIREBASE_STORAGE_BUCKET: "fir-56958.appspot.com",
+    FIREBASE_MESSAGING_SENDER_ID: "45989334453",
+    FIREBASE_APP_ID: "1:45989334453:web:7d74839c7699d177ee5dfc"
+  }
 }
 
 module.exports = nextConfig
