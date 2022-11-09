0566b8d95c81efe27a8557aa9cc2e3a02a2863eb
morshidulrahman <boydanger416@gmail.com>
2022-11-09 20:30:29 +0600

    error-fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
 import '../styles/globals.css'
-import { SessionProvider } from 'next-auth/react'
+
 import { store } from '../app/redux/Store'
 import { Provider } from 'react-redux'
 
 function MyApp({ Component, pageProps, session }) {
   return (
-    <SessionProvider session={session}>
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
-    </SessionProvider>
+
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+
   )
 }
 
